4cd8c18a16a0686e72a25ce323b69d8d1d6c7713
Dorthy P Thielsen <dotinmotionproductions@gmail.com>
2021-05-17 17:30:09 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,7 +61,12 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.to_s.capitalize!}:"
     holiday.each do |name, supplies|
-      puts "#{name.to_s.capitalize!}: #{supplies.flatten}"
+      name.split
+      name.capitalize!
+      name.join
+      supplies.flatten
+      supplies.join
+      puts "#{name}: #{supplies}"
     end
   end
 end
