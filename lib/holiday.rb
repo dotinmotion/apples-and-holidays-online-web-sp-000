70c38d193313cc90235807711df04b7912cb4c5e
Dorthy P Thielsen <dotinmotionproductions@gmail.com>
2021-05-17 17:05:35 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,7 +58,9 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  holiday_hash.each.flatten
+  holiday_hash.each do |season, holiday|
+    season.to_s.capitalize!.join
+    binding.pry
 end
 
 def all_holidays_with_bbq(holiday_hash)
