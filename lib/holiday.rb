ecd282cb72ff6b9d6487e317ca2ac4a1cc27ca8e
Dorthy P Thielsen <dotinmotionproductions@gmail.com>
2021-05-17 17:05:40 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,6 +61,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     season.to_s.capitalize!.join
     binding.pry
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
